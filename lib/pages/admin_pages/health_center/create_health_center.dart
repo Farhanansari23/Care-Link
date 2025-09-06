bd51de5b1a7fb0fea42772eb91e8992a101c8869
Farhan Ansari <drscorpion4@gmail.com>
2025-09-06 16:32:04 +0545

    FA-26:working on admin dashboard

---
+++ lib/pages/admin_pages/health_center/create_health_center.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AdminCreateHealthCenterPage extends StatefulWidget {
+  const AdminCreateHealthCenterPage({super.key});
+
+  @override
+  State<AdminCreateHealthCenterPage> createState() => _AdminCreateHealthCenterPageState();
+}
+
+class _AdminCreateHealthCenterPageState extends State<AdminCreateHealthCenterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('coming soon'),
+        ],
+      ),
+    );
+  }
+}
